6a53f8344b1405f7f41f2f7d470f7c950a0d424a
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-11 18:51:52 -0300

    ♻️ ModalEditFood refactored Now ModalEditFood is written in TypeScript as a functional component. ModalEditFood uses ReactJS Refs and TypeScript typings.

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,56 @@
+import { useRef } from "react";
+import { FiCheckSquare } from "react-icons/fi";
+
+import { Form } from "./styles";
+import Modal from "../Modal";
+import { Input } from "../Input";
+
+interface FoodProps {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string;
+}
+interface ModalEditFoodProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  editingFood: FoodProps;
+  handleUpdateFood: (food: FoodProps) => void;
+}
+
+export function ModalEditFood({
+  isOpen,
+  setIsOpen,
+  editingFood,
+  handleUpdateFood,
+}: ModalEditFoodProps) {
+  const formRef = useRef(null);
+
+  async function handleSubmit(data: FoodProps) {
+    handleUpdateFood(data);
+    setIsOpen();
+  }
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
