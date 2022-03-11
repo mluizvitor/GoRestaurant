b40b25991583c8a31cbb7c0ea7346c6630821793
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-11 16:24:49 -0300

    :sparkles: Initial TypeScript support

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Routes from "./routes";
+
+import GlobalStyle from "./styles/global";
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  );
+}
+
+export default App;
