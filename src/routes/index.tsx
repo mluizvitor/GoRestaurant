8df81f6b0aa58e2f5c130a2a5f6d04047186e8e9
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-11 16:31:35 -0300

    :recycle: Routes and API service refactored Now they are writen in typescript Routes now is a function and not a Class

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { Switch, Route } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard";
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+    </Switch>
+  );
+}
+
+export default Routes;
